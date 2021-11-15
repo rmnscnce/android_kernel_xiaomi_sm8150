eb753d5e92e4127ab6bfafbffc589d44b072b14f
Eric Dumazet <edumazet@google.com>
2021-11-15 11:02:34 -0800

    net: remove sk_route_forced_caps
    We were only using one bit, and we can replace it by sk_is_tcp()
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Acked-by: Soheil Hassas Yeganeh <soheil@google.com>
    Acked-by: Arjun Roy <arjunroy@google.com>
    Signed-off-by: Mo 森 <rmnscnce@ya.ru>

--- net/core/sock.c
+++ net/core/sock.c
@@ -1789,6 +1789,8 @@ void sk_setup_caps(struct sock *sk, struct dst_entry *dst)
 
 	sk_dst_set(sk, dst);
 	sk->sk_route_caps = dst->dev->features;
+	if (sk_is_tcp(sk))
+		sk->sk_route_caps |= NETIF_F_GSO;
 	if (sk->sk_route_caps & NETIF_F_GSO)
 		sk->sk_route_caps |= NETIF_F_GSO_SOFTWARE;
 	sk->sk_route_caps &= ~sk->sk_route_nocaps;
